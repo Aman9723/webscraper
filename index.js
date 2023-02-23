b391da96b99c38681556bbc45cee8de086de57a0
Aman Singh Chauhan <amansingh9723chauhan@gmail.com>
2023-02-23 15:11:44 +0530

    error handling, exe added

--- index.js
+++ index.js
@@ -10,82 +10,20 @@ for (let i = 0; i < websites.length; i++) {
 }
 
 async function configureBrowser() {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     fs.writeFileSync('data.csv', `Website,Property,Value\n`);
 
     let i = 0;
     scrape();
     async function scrape() {
-        if (i == websites.length) {
-            await browser.close();
-            return;
-        }
-        await page.goto(websites[i]);
-        const {
-            appleMobileWebAppCapable,
-            description,
-            keywords,
-            ogDescription,
-            ogImage,
-            ogTitle,
-            ogType,
-            ogUrl,
-            twitterCard,
-            twitterDescription,
-            twitterTitle,
-            viewport,
-            title,
-        } = await page.evaluate(() => {
-            const metaTags = document.getElementsByTagName('meta');
-            const title = document.getElementsByTagName('title')[0].innerText;
-
-            let appleMobileWebAppCapable = '';
-            let description = '';
-            let keywords = '';
-            let ogDescription = '';
-            let ogImage = '';
-            let ogTitle = '';
-            let ogType = '';
-            let ogUrl = '';
-            let twitterCard = '';
-            let twitterDescription = '';
-            let twitterTitle = '';
-            let viewport = '';
-
-            for (let i = 0; i < metaTags.length; i++) {
-                const name = metaTags[i].getAttribute('name');
-                const property = metaTags[i].getAttribute('property');
-
-                if (name === 'apple-mobile-web-app-capable') {
-                    appleMobileWebAppCapable =
-                        metaTags[i].getAttribute('content');
-                } else if (name === 'description') {
-                    description = metaTags[i].getAttribute('content');
-                } else if (name === 'keywords') {
-                    keywords = metaTags[i].getAttribute('content');
-                } else if (property === 'og:description') {
-                    ogDescription = metaTags[i].getAttribute('content');
-                } else if (property === 'og:image') {
-                    ogImage = metaTags[i].getAttribute('content');
-                } else if (property === 'og:title') {
-                    ogTitle = metaTags[i].getAttribute('content');
-                } else if (property === 'og:type') {
-                    ogType = metaTags[i].getAttribute('content');
-                } else if (property === 'og:url') {
-                    ogUrl = metaTags[i].getAttribute('content');
-                } else if (property === 'twitter:card') {
-                    twitterCard = metaTags[i].getAttribute('content');
-                } else if (property === 'twitter:description') {
-                    twitterDescription = metaTags[i].getAttribute('content');
-                } else if (property === 'twitter:title') {
-                    twitterTitle = metaTags[i].getAttribute('content');
-                } else if (name === 'viewport') {
-                    viewport = metaTags[i].getAttribute('content');
-                }
+        try {
+            if (i == websites.length) {
+                await browser.close();
+                return;
             }
-
-            return {
+            await page.goto(websites[i]);
+            const {
                 appleMobileWebAppCapable,
                 description,
                 keywords,
@@ -99,29 +37,103 @@ async function configureBrowser() {
                 twitterTitle,
                 viewport,
                 title,
-            };
-        });
+            } = await page.evaluate(() => {
+                const metaTags = document.getElementsByTagName('meta');
+                const title =
+                    document.getElementsByTagName('title')[0].innerText;
 
-        fs.appendFileSync(
-            'data.csv',
-            `${websites[i]},apple-mobile-web-app-capable,"${appleMobileWebAppCapable}"
-            ,description,"${description}"
-            ,keywords,"${keywords}"
-            ,ogDescription,"${ogDescription}"
-            ,ogImage,"${ogImage}"
-            ,ogTitle,"${ogTitle}"
-            ,ogType,"${ogType}"
-            ,ogUrl,"${ogUrl}"
-            ,twitterCard,"${twitterCard}"
-            ,twitterDescription,"${twitterDescription}"
-            ,twitterTitle,"${twitterTitle}"
-            ,viewport,"${viewport}"
-            ,title,"${title}"\n`
-        );
-        i++;
-        setTimeout(() => {
-            scrape();
-        }, delay * 1000);
+                let appleMobileWebAppCapable = '';
+                let description = '';
+                let keywords = '';
+                let ogDescription = '';
+                let ogImage = '';
+                let ogTitle = '';
+                let ogType = '';
+                let ogUrl = '';
+                let twitterCard = '';
+                let twitterDescription = '';
+                let twitterTitle = '';
+                let viewport = '';
+
+                for (let i = 0; i < metaTags.length; i++) {
+                    const name = metaTags[i].getAttribute('name');
+                    const property = metaTags[i].getAttribute('property');
+
+                    if (name === 'apple-mobile-web-app-capable') {
+                        appleMobileWebAppCapable =
+                            metaTags[i].getAttribute('content');
+                    } else if (name === 'description') {
+                        description = metaTags[i].getAttribute('content');
+                    } else if (name === 'keywords') {
+                        keywords = metaTags[i].getAttribute('content');
+                    } else if (property === 'og:description') {
+                        ogDescription = metaTags[i].getAttribute('content');
+                    } else if (property === 'og:image') {
+                        ogImage = metaTags[i].getAttribute('content');
+                    } else if (property === 'og:title') {
+                        ogTitle = metaTags[i].getAttribute('content');
+                    } else if (property === 'og:type') {
+                        ogType = metaTags[i].getAttribute('content');
+                    } else if (property === 'og:url') {
+                        ogUrl = metaTags[i].getAttribute('content');
+                    } else if (property === 'twitter:card') {
+                        twitterCard = metaTags[i].getAttribute('content');
+                    } else if (property === 'twitter:description') {
+                        twitterDescription =
+                            metaTags[i].getAttribute('content');
+                    } else if (property === 'twitter:title') {
+                        twitterTitle = metaTags[i].getAttribute('content');
+                    } else if (name === 'viewport') {
+                        viewport = metaTags[i].getAttribute('content');
+                    }
+                }
+
+                return {
+                    appleMobileWebAppCapable,
+                    description,
+                    keywords,
+                    ogDescription,
+                    ogImage,
+                    ogTitle,
+                    ogType,
+                    ogUrl,
+                    twitterCard,
+                    twitterDescription,
+                    twitterTitle,
+                    viewport,
+                    title,
+                };
+            });
+
+            fs.appendFileSync(
+                'data.csv',
+                `${websites[i]},apple-mobile-web-app-capable,"${appleMobileWebAppCapable}"
+                ,description,"${description}"
+                ,keywords,"${keywords}"
+                ,ogDescription,"${ogDescription}"
+                ,ogImage,"${ogImage}"
+                ,ogTitle,"${ogTitle}"
+                ,ogType,"${ogType}"
+                ,ogUrl,"${ogUrl}"
+                ,twitterCard,"${twitterCard}"
+                ,twitterDescription,"${twitterDescription}"
+                ,twitterTitle,"${twitterTitle}"
+                ,viewport,"${viewport}"
+                ,title,"${title}"\n`
+            );
+
+            i++;
+            setTimeout(() => {
+                scrape();
+            }, delay * 1000);
+        } catch (err) {
+            console.log(`${websites[i]} Error:`, err.message);
+
+            i++;
+            setTimeout(() => {
+                scrape();
+            }, delay * 1000);
+        }
     }
 }
 
